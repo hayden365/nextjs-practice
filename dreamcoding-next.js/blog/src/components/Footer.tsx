f60dac521f53ab0f27a5b67b3131e9cdcd63c187
hayden365 <ysmh100@gmail.com>
2023-04-17 23:56:06 +0900

    Feat: 전체layout

---
+++ dreamcoding-next.js/blog/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Footer = (props: Props) => {
+	return <footer>{"푸터입니다. 혜지's 푸터"}</footer>;
+};
+
+export default Footer;
---
+++ dreamcoding-next.js/blog/src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Link from "next/link";
+
+export default function Header() {
+	return (
+		<header>
+			<Link href={"/"}>
+				<h1>{"hyeji's Blog"}</h1>
+			</Link>
+			<nav>
+				<Link href="/">home</Link>
+				<Link href="/about">about</Link>
+				<Link href="/posts">posts</Link>
+				<Link href="/contact">contact</Link>
+			</nav>
+		</header>
+	);
+}
