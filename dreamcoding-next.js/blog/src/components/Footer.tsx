239517de8abd89863828cde6294f2d3da978abe1
hayden365 <ysmh100@gmail.com>
2023-04-18 22:04:54 +0900

    Feat: Homepage 내용추가

--- dreamcoding-next.js/blog/src/components/Footer.tsx
+++ dreamcoding-next.js/blog/src/components/Footer.tsx
@@ -3,7 +3,11 @@ import React from "react";
 type Props = {};
 
 const Footer = (props: Props) => {
-	return <footer>{"푸터입니다. 혜지's 푸터"}</footer>;
+	return (
+		<footer className="bg-slate-900 text-white py-2 text-sm text-center">
+			{"푸터입니다. 혜지's 푸터"}
+		</footer>
+	);
 };
 
 export default Footer;
---
+++ dreamcoding-next.js/blog/src/components/Hero.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import React from "react";
+import profileImage from "../../public/images/profile.png";
+import Link from "next/link";
+
+export default function Hero() {
+	return (
+		<section className="text-center">
+			<Image
+				className="rounded-full mx-auto"
+				src={profileImage}
+				alt="Picture of the author"
+				width={250}
+				height={250}
+			/>
+			<h2 className="text-3xl font-bold mt-2">{"Hi, I'm Hyeji"}</h2>
+			<h3 className="text-xl font-semibold">Front-end Engineer</h3>
+			<p>재미로 코딩하는 사람, 재미코딩</p>
+			<Link href="/contact">
+				<button>Contact Me</button>
+			</Link>
+		</section>
+	);
+}
