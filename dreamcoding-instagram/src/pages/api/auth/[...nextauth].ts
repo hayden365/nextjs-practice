39ab14fd382c19e8f2b5c5d235b488e43a6b8553
hayden365 <ysmh100@gmail.com>
2023-05-01 06:36:25 +0900

    Feat: Connect Sanity with signin

--- dreamcoding-instagram/src/pages/api/auth/[...nextauth].ts
+++ dreamcoding-instagram/src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,44 @@
-import NextAuth from "next-auth";
+import { addUser } from "@/service/user";
+import { log } from "console";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import { signIn } from "next-auth/react";
 
-export const authOptions = {
+export const authOptions: NextAuthOptions = {
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_OAUTH_ID || "",
 			clientSecret: process.env.GOOGLE_OAUTH_SECRET || "",
 		}),
 	],
+	callbacks: {
+		async signIn({ user: { id, name, image, email } }) {
+			if (!email) {
+				return false;
+			}
+			addUser({
+				id,
+				name: name || "",
+				image,
+				email,
+				username: email.split("@")[0],
+			});
+			return true;
+		},
+		async session({ session }) {
+			console.log(session);
+
+			const user = session?.user;
+			if (user) {
+				session.user = {
+					...user,
+					username: user.email?.split("@")[0] || "",
+				};
+			}
+
+			return session;
+		},
+	},
 	pages: {
 		signIn: "/auth/signin",
 	},
